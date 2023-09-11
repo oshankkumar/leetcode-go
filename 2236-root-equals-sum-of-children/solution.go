eb23a951c9020026dc577cd72b40428a009a3d2d
oshankkumar <oshankfriends@gmail.com>
2023-09-11 16:01:22 +0530

    617 Merge two binary tree

---
+++ 2236-root-equals-sum-of-children/solution.go
@@ -0,0 +1,11 @@
+package rootequalssumofchildren
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func checkTree(root *TreeNode) bool {
+	return root.Val == root.Left.Val+root.Right.Val
+}
